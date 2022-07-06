a21ac81a187af2d8a28aa9556f7f0e2aeb77cc65
climbTheStairs <yxtqwf+github@gmail.com>
2022-07-06 13:58:39 -0700

    simplify password hashing function

--- login.go
+++ login.go
@@ -3,15 +3,12 @@ package main
 import (
 	"crypto/md5"
 	"errors"
-	"fmt"
 	"net/http"
 	"time"
 )
 
-func md5sum(s string) string {
-	h := md5.New()
-	fmt.Fprint(h, s)
-	return fmt.Sprintf("%x", h.Sum(nil))
+func hash(s string) [md5.Size]byte {
+	return md5.Sum([]byte(s))
 }
 
 func register(w http.ResponseWriter, r *http.Request, info *sessionInfo) {
@@ -40,7 +37,7 @@ func register(w http.ResponseWriter, r *http.Request, info *sessionInfo) {
 		return
 	}
 		
-	accounts[username] = md5sum(password)
+	accounts[username] = hash(password)
 	http.Redirect(w, r, "/login", http.StatusFound)
 }
 
@@ -113,5 +110,5 @@ func verifyUsername(username string) bool {
 func verifyPassword(username, password string) bool {
 	// verifyPassword automatically verifies username as well
 	correctPassword, ok := accounts[username]
-	return ok && md5sum(password) == correctPassword
+	return ok && hash(password) == correctPassword
 }
